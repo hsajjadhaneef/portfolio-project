0e812fabcc9dcc59046fef5c63d777f03b4775cc
hsajjadhaneef <136099131+hsajjadhaneef@users.noreply.github.com>
2023-09-12 07:47:17 -0700

    Add files via upload

---
+++ yearly_sales_report for croma.sql
@@ -0,0 +1,17 @@
+# Generate a yearly report for Croma India where there are two columns
+
+--	1. Fiscal Year
+--	2. Total Gross Sales amount In that year from Croma
+
+SET SESSION sql_mode = '';
+
+SELECT get_fiscal_year(s.date) as fiscal_year,round(sum(g.gross_price * S.sold_quantity),2)
+ as gross_price_total FROM fact_sales_monthly S
+JOIN fact_gross_price G ON S.product_code=G.product_code
+AND G.fiscal_year=get_fiscal_year(s.date)
+
+WHERE customer_code=90002002
+group by fiscal_year
+order by fiscal_year asc;
+
+

a92d0f94d109010383cb0adc74adad2713f2a760
hsajjadhaneef <136099131+hsajjadhaneef@users.noreply.github.com>
2023-09-12 07:46:13 -0700

    Add files via upload

---
+++ supply chain analytics.sql
@@ -0,0 +1,10 @@
+#  As a product owner i need an aggregate forecast accuarcy report for all 
+-- the customers for a given fiscal year so that i can track accuaracy 
+-- of the forecast we make for these customers 
+---- the report should have the following fields
+-- 1. customer_code,market,name
+-- 2. total_sold quantity 
+-- 3. total forecast quantity 
+-- 4. net error 
+-- 5. absolute error
+-- 6. forecast accuracy %
